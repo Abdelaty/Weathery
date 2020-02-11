1249eaff012382b0640128cca0008637805e3bc3
abdelaty <abdelaty.mohammedmagdi@gmail.com>
2020-02-11 10:10:08 +0200

    -Add Local Db Room. -Send units parameters. -Lesson 3
    Signed-off-by: abdelaty <abdelaty.mohammedmagdi@gmail.com>

--- app/src/main/java/com/example/forecastmvvm/data/db/entity/CurrentWeatherEntry.kt
+++ app/src/main/java/com/example/forecastmvvm/data/db/entity/CurrentWeatherEntry.kt
@@ -1,6 +1,7 @@
 package com.example.forecastmvvm.data.db.entity
 
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
@@ -9,21 +10,20 @@ const val CURRENT_WEATHER_ID = 0
 
 @Entity(tableName = "current_weather")
 data class CurrentWeatherEntry(
-    val cloudcover: Int,
+    @SerializedName("temperature")
+    val temperature: Double,
     @SerializedName("feelslike")
     val feelslike: Double,
     @SerializedName("is_day")
     val isDay: String,
-    val precip: Double,
-    @SerializedName("temperature")
-    val tempC: Double,
     @SerializedName("uv_index")
     val uvIndex: Double,
-    @SerializedName("wind_dir")
-    val windDir: String,
     @SerializedName("wind_speed")
-    val windSpeed: Double
+    val windSpeed: Double,
+    @SerializedName("wind_dir")
+    val windDirection: String
 ) {
     @PrimaryKey(autoGenerate = false)
+    @ColumnInfo
     var id: Int = CURRENT_WEATHER_ID
 }
